79b93036ff5f7af9fdfc5630339ef80a39538c20
Joep Dumont <joep@dumont.nl>
2020-12-04 14:44:48 +0100

    implemented nightly split_once

--- 4/solution.rs
+++ 4/solution.rs
@@ -1,8 +1,7 @@
+#![feature(str_split_once)]
+
 use std::fs;
 use std::time::Instant;
-
-const PP_REQS: [&'static str; 7]  = 
-    ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
 
 fn main() {
     let filename = "input";
@@ -23,14 +22,12 @@ fn part1(input: &str) -> usize {
     return input
         .split("\n\n")
         .filter(|passport| {
-                let values: Vec<String> = passport
-                                        .split_whitespace()
-                                        .map(|s| s
-                                                .chars()
-                                                .take(3)
-                                                .collect())
-                                        .collect();
-                return PP_REQS.iter().all(|val| values.contains(&val.to_string()))
+                passport
+                    .split_whitespace()
+                    .filter_map(|s| s.split_once(":"))
+                    .filter(|(key, _val)| 
+                        "cid" != *key)
+                    .count() >= 7
         })
         .count();
 }
@@ -41,29 +38,29 @@ fn part2(input: &str) -> usize {
         .filter(|passport| 
             passport
             .split_whitespace()
-            .map(|s| s.split(":").collect::<Vec<&str>>())
-            .filter(|val| 
-                match val[0] {
-                    "byr" => (val[1].parse::<usize>().unwrap() >= 1920 && val[1].parse::<usize>().unwrap() <= 2002),
-                    "iyr" => (val[1].parse::<usize>().unwrap() >= 2010 && val[1].parse::<usize>().unwrap() <= 2020),
-                    "eyr" => (val[1].parse::<usize>().unwrap() >= 2020 && val[1].parse::<usize>().unwrap() <= 2030),
+            .filter_map(|s| s.split_once(":"))
+            .filter(|&(key, val)| 
+                match key {
+                    "byr" => (val.parse::<usize>().unwrap() >= 1920 && val.parse::<usize>().unwrap() <= 2002),
+                    "iyr" => (val.parse::<usize>().unwrap() >= 2010 && val.parse::<usize>().unwrap() <= 2020),
+                    "eyr" => (val.parse::<usize>().unwrap() >= 2020 && val.parse::<usize>().unwrap() <= 2030),
                     "hgt" => {
-                        if val[1].ends_with("in") {
-                            let size = val[1].chars().take(val[1].len()-2).collect::<String>().parse::<usize>().unwrap();
+                        if val.ends_with("in") {
+                            let size = val.chars().take(val.len()-2).collect::<String>().parse::<usize>().unwrap();
                             size >= 59 && size <= 76
                         }
-                        else if val[1].ends_with("cm") {
-                            let size = val[1].chars().take(val[1].len()-2).collect::<String>().parse::<usize>().unwrap();
+                        else if val.ends_with("cm") {
+                            let size = val.chars().take(val.len()-2).collect::<String>().parse::<usize>().unwrap();
                             size >= 150 && size <= 193
                         }
                         else {
                             false
                         }
                     }
-                    "hcl" => val[1].as_bytes()[0] == b'#' 
-                          && val[1].bytes().skip(1).all(|b| (b >= b'0' && b <= b'9') || (b >= b'a' && b <= b'f')),
-                    "ecl" => ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(&val[1]),
-                    "pid" => val[1].bytes().all(|b| (b>= b'0' && b <= b'9' )) && val[1].len()==9,
+                    "hcl" => val.as_bytes()[0] == b'#' 
+                          && val.bytes().skip(1).all(|b| b.is_ascii_hexdigit()),
+                    "ecl" => ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(&val),
+                    "pid" => val.bytes().all(|b| b.is_ascii_digit()) && val.len()==9,
                     _ => false,
                 }
             ).count() == 7
