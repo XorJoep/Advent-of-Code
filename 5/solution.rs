6050b44e2a4a9645f9f4cb498706569b5bbd07f6
Joep Dumont <joep@dumont.nl>
2020-12-05 13:40:36 +0100

    small refactors

--- 5/solution.rs
+++ 5/solution.rs
@@ -1,58 +1,46 @@
+use std::collections::HashSet;
 use std::fs;
 use std::time::Instant;
-use std::collections::HashSet;
-// use std::iter::FromIterator;
-
-
 
 fn main() {
     let filename = "input";
-    // let filename = "input";
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
-
+    // let filename = "ex_input";
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
     let start = Instant::now();
     println!("Solution for PART 1: {}", part1(&contents));
-    println!("Finished after {:?}", start.elapsed());   
+    println!("Finished after {:?}", start.elapsed());
 
     let start = Instant::now();
     println!("Solution for PART 2: {}", part2(&contents));
-    println!("Finished after {:?}", start.elapsed());   
+    println!("Finished after {:?}", start.elapsed());
 }
 
 fn convert(code: &str) -> usize {
-    let val = code.chars().map(|c|
-        match c {
-            'F' => '0',
-            'B' => '1',
-            'L' => '0',
-            'R' => '1',
-            _ => 'X'
-        }
-    ).collect::<String>();
-    usize::from_str_radix(&val, 2).unwrap()
+    code
+        .bytes().rev()
+        .enumerate()
+        .fold(0, |acc, (i, b)| (((b==b'B' || b == b'R') as usize) << i) + acc)
 }
 
 fn part1(input: &str) -> usize {
-    return input
-            .lines()
-            .map(|s| s.split_at(7))
-            .map(|(row, col)| {
-                convert(row)*8+convert(col)
-            })
-            .fold(0, |acc, seat_id| acc.max(seat_id)); // 20 minutes
+    input
+        .lines()
+        .map(|s| s.split_at(7))
+        .map(|(row, col)| convert(row) * 8 + convert(col))
+        .max().unwrap()
 }
 
 fn part2(input: &str) -> usize {
-    let seats_taken: HashSet<usize> = 
-            input
-            .lines()
-            .map(|s| s.split_at(7))
-            .map(|(row, col)| {
-                convert(row)*8+convert(col)
-            })
-            .collect();
-    let seats_available = (0..807 as usize).collect::<HashSet<usize>>();
-    let remaining_seats = seats_available.difference(&seats_taken);
-    remaining_seats.into_iter().fold(0, |acc, seat| acc.max(*seat))
+    let seats_taken: HashSet<usize> = input
+        .lines()
+        .map(|s| s.split_at(7))
+        .map(|(row, col)| convert(row) * 8 + convert(col))
+        .collect();
+
+    let min_id: usize = *seats_taken.iter().min().unwrap();
+    let max_id: usize = *seats_taken.iter().max().unwrap();
+
+    *(min_id..max_id as usize).collect::<HashSet<usize>>()
+        .difference(&seats_taken)
+        .next().unwrap()
 }
