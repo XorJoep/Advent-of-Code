ac802c8e26a73dd3fd42b28a249e7d6afa416e14
Joep <xorjoep@gmail.com>
2022-12-03 22:10:26 +0100

    add ex_input to git

--- 2022/00/sol.rs
+++ 2022/00/sol.rs
@@ -3,25 +3,25 @@ use std::time::Instant;
 
 fn main() {
     let expect_result_part1 = 
-        1;
+        2;
     let expect_result_part2 = 
         1;
     
     let filename_example = "ex_input";
-    let filename= "input";
+    let filename = "input";
     
-    let contents_example = fs::read_to_string(filename_example).expect("Something went wrong reading the example file");
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the example file");
+    let contents_example = fs::read_to_string(filename_example).expect("Erro reading the EXAMPLE file");
+    let contents = fs::read_to_string(filename).expect("ERROR reading the INPUT file");
 
     let mut succes;
     println!("Part 1 - Example");
     succes = execute_part(part1, &contents_example, expect_result_part1);
     println!("Part 1");
-    succes = succes && execute_part(part1, &contents, 0);
+    succes &= execute_part(part1, &contents, 0);
     println!("Part 2 - Example");
-    succes = succes && execute_part(part2, &contents_example, expect_result_part2);
+    succes &= execute_part(part2, &contents_example, expect_result_part2);
     println!("Part 2");
-    let _ = succes && execute_part(part2, &contents, 0);
+    execute_part(part2, &contents, 0);
 }
 
 fn execute_part(part_fn: fn(&str) -> u32, input: &str, example_result: u32) -> bool {
@@ -31,20 +31,18 @@ fn execute_part(part_fn: fn(&str) -> u32, input: &str, example_result: u32) -> b
     println!("\tFinished after {:?}", start.elapsed());
     println!("\tSolution found: {}", result);
 
-    let succes;
     if example_result == 0 {
-        succes = true;
+        println!("\tSkipping check");
+        true
     }
-    else if example_result != 0 && result != example_result {
+    else if result != example_result {
         println!("\tINCORRECT: Expected: [{}] but got [{}]", example_result, result);
-        succes = false
+        false
     }
     else {
-        println!("\tExample CORRECT");
-        succes = true;
+        println!("\tCORRECT");
+        true
     }
-
-    succes
 }
 
 
