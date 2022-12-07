ddbf32b34387d58532fd17857dd0409bf15814a3
Joep Dumont <xorjoep@gmail.com>
2022-12-07 23:12:49 +0100

    can parse

--- 2022/07/sol.rs
+++ 2022/07/sol.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::time::Instant;
+use std::collections::HashMap;
 
 fn main() {
     let expect_result_part1 = 95437;
@@ -45,44 +46,50 @@ fn execute_part(part_fn: fn(&str) -> u32, input: &str, example_result: u32) -> b
 
 #[derive(Debug)]
 struct FileNode {
+    id: usize,
     name: String,
     size: u64,
+    files: Option<Vec<usize>>,
+    parent: Option<usize>,
 }
 
-#[derive(Debug)]
-struct DirNode {
-    files: Vec<FileNode>,
-    folders: Vec<Box<DirNode>>,
-    name: String,
-    parent: Option<Box<DirNode>>,
-}
-
-impl DirNode {
-    pub fn new(name: &str, parent: Option<Box<DirNode>>) -> Self {
-        DirNode {
-            files: vec![],
-            folders: vec![],
+impl FileNode {
+    pub fn new_file(id: usize, name: &str, parent: Option<usize>, size: u64) -> Self {
+        FileNode {
+            id: id,
             name: name.to_string(),
             parent: parent,
+            size: size,
+            files: None,
         }
     }
 
-    pub fn add_folder(&self, name: &str) {
-        let new_node = DirNode::new(name, Some(Box::new(self)));
-        self.folders.push(Box::new(new_node));
+    pub fn new_folder(id: usize, name: &str, parent: Option<usize>) -> Self {
+        FileNode {
+            id: id,
+            name: name.to_string(),
+            parent: parent,
+            size: 0,
+            files: Some(vec![]),
+        }
     }
 
-    pub fn add_file(&self, name: &str, size: u64) {
-        self.files.push(FileNode {
-            name: name.to_string(),
-            size: size
-        })
+    pub fn update_size(mut self) -> u64 {
+        match files {
+            Some(_) => files.iter().map(|file| file.calc_size()).sum(),
+            None => self.size
+        }
     }
+
+    // pub fn add_folder(stack: &mut Vec<FileNode>, name: &str, parent: Option<usize>) {}
 }
 
 fn part1(input: &str) -> u32 {
-    let dir = DirNode::new("/", None);
-    println!("{:?}", dir);
+    let mut stack: Vec<FileNode> = vec![FileNode::new_folder(0, "/", None)];
+    let mut dir_map = HashMap::new();
+    let mut cur_dir = 0;
+
+    // println!("{:?}", stack);
     input
         .split("$ ")
         .skip(2) // skip line 1. We know the topdir is '/'
@@ -90,20 +97,64 @@ fn part1(input: &str) -> u32 {
         .for_each(|(instr, output)| {
             match instr {
                 "ls" => {
-                    output.lines().filter_map(|line| line.split_once(" ")).for_each(|(file, name)| {
-                        match file {
-                            "dir" => dir.add_folder(name),
-                            _ => dir.add_file(name, file.parse::<u64>().expect("not an int")),
-                        }
-                    })
-                },
-                "cd .." => (),
-                _ => (),
-            };
-            println!("{:?}: {:?}", instr, output)
+                    output
+                        .lines()
+                        .filter_map(|line| line.split_once(" "))
+                        .for_each(|(file, name)| {
+                            let new_id = stack.len();
+                            match file {
+                                "dir" => {
+                                    stack[cur_dir].files
+                                        .as_mut()
+                                        .expect("cannot push to file")
+                                        .push(new_id);
+                                    stack.push(FileNode::new_folder(new_id, name, Some(cur_dir)));
+                                    dir_map.insert(name.to_string(), new_id);
+                                }
+                                _ => {
+                                    stack[cur_dir].files
+                                        .as_mut()
+                                        .expect("cannot push to file")
+                                        .push(new_id);
+                                    stack.push(
+                                        FileNode::new_file(
+                                            new_id,
+                                            name,
+                                            Some(stack[cur_dir].id),
+                                            file.parse::<u64>().expect("not an int")
+                                        )
+                                    );
+                                }
+                            }
+                        });
+                }
+                "cd .." => {
+                    cur_dir = stack[cur_dir].parent.unwrap();
+                }
+                _ => {
+                    // cd xxx
+                    let (_, dirname) = instr.split_once(" ").unwrap();
+                    cur_dir = *dir_map.get(dirname).expect("could not find dir");
+                }
+            }
         });
 
-    1
+    println!("{:?}", stack);
+
+    
+
+    // stack
+    //     .iter()
+    //     .filter(|node| node.size == 0)
+    //     .filter_map(|folder| folder.files.as_ref())
+    //     .map(|files|
+    //         files
+    //             .iter()
+    //             .map(|id| stack[*id].size)
+    //             .sum::<u64>()
+    //     )
+    //     .filter(|&folder_size| folder_size < 100000)
+    //     .sum::<u64>() as u32
 }
 
 fn part2(input: &str) -> u32 {
